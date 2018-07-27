17cac6950bd272e68a4a950745de7edca49be979
Alfie Darko <mrhazzy@gmail.com>
2018-07-27 14:31:55 +0100

    added boilerplate

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+// Create a new compononent. This component should produce some HTML
+const App = function() {
+  return <div> Hi! </div>;
+};
+// tAKE THIS COMPONENTS html and put it on the page (in the DOM)
