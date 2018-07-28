4d4d79f7aefa8a6436d86847f7aaaa28083d3191
Alfie Darko <mrhazzy@gmail.com>
2018-07-28 09:31:49 +0100

    added state text input to the search_bar component

---
+++ src/components/search_bar.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+// every react class component must have a render method
+
+/* 
+   class vs functional component? 
+   start off with functional and then if you need state, props etc.
+   then refactor 
+*/
+
+class SearchBar extends Component {
+  // constructor is for setup ie. initialising variables and state etc.
+  constructor(props) {
+    super(props);
+    // super used to access call functions on object's parent
+
+    /* 
+      when you want to acces this.props
+      child class constructor cannot make use of this until super
+      has been called 
+    */
+
+    this.state = { term: '' };
+    /* 
+      Only inside of the constructor function 
+      will we set our state like so
+   */
+  }
+
+  render() {
+    // this.state.term - event.target.value // BAD!!! DONT DO THIS!!
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+        Value of the input: {this.state.term}
+      </div>
+    );
+  }
+  // eventHandlers run when an event occurs,
+  // on this instance whenever the input changes
+
+  // all html elements emmit a change event, we can tap into this.
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyD-MCZtP_2HekQJ34j6OL8BLn7U6Sbeock';
 
+YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
+  console.log(data);
+});
 // Create a new compononent. This component should produce some HTML
 const App = () => {
-  return <div> Hi! </div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 // Take this html and put it on the page (in the DOM)
 //  first argument takes a component, second takes a selected div
