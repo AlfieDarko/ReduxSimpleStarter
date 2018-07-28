536fc94c285fd7ccb2fe57aa3bd5a33227f6a76d
Alfie Darko <mrhazzy@gmail.com>
2018-07-28 13:04:33 +0100

    added video detail component to the app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyD-MCZtP_2HekQJ34j6OL8BLn7U6Sbeock';
 
 // Create a new compononent. This component should produce some HTML
@@ -20,6 +21,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
